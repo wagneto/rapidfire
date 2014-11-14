dc0af0a163e4e757a996227ef7a48f5fa39e8b4a
Wagner Matos <eu@wagneto.com.br>
2014-11-14 01:14:43 -0200

    Replicando valores de questões para novas pesquisas

--- app/controllers/rapidfire/question_groups_controller.rb
+++ app/controllers/rapidfire/question_groups_controller.rb
@@ -5,7 +5,8 @@ module Rapidfire
     respond_to :json, only: :results
 
     def index
-      @question_groups = QuestionGroup.all
+      @question_groups = QuestionGroup.search(params[:search])
+      #@question_groups = QuestionGroup.all
       respond_with(@question_groups)
     end
 
@@ -15,14 +16,46 @@ module Rapidfire
     end
 
     def create
+      #raise Question.find_by_question_group_id(13).question_text.inspect
+      #@test = Question.find_by_question_group_id(13)
+      #@test = Question.find(9, 10, 11, 12, 13)    # returns an array for objects with IDs in (1, 2, 6)
+
+      #raise @test[1].question_text.inspect
+
       @question_group = QuestionGroup.new(question_group_params)
       @question_group.save
+
+      #raise QuestionGroup.last.id.inspect
+
+      @question = Question.new(question_group_id: QuestionGroup.last.id.inspect, type: "Rapidfire::Questions::Radio", question_text: "CONTEÚDO DO COMPONENTE - Contribuição para o seu desempenho profissional", position: "1", answer_options: "MUITO BOM
+BOM
+REGULAR
+DEFICIENTE")
+
+      @question1 = Question.new(question_group_id: QuestionGroup.last.id.inspect, type: "Rapidfire::Questions::Radio", question_text: "CONTEÚDO DO COMPONENTE - Qualidade e quantidade das informações atenderam suas expectativas", position: "2", answer_options: "MUITO BOM
+BOM
+REGULAR
+DEFICIENTE")
+
+      @question.save
+      @question1.save
 
       respond_with(@question_group, location: rapidfire.question_groups_url)
     end
 
     def edit
       @question_group = QuestionGroup.find(params[:id])
+      respond_with(@question_group)
+    end
+
+    def update
+      @question_group = QuestionGroup.find(params[:id])
+      
+      if @question_group.update(question_group_params)
+        respond_with(@question_group, location: rapidfire.question_groups_url)      
+      else
+        respond_with(@question_group, location: rapidfire.question_groups_url)      
+      end
     end
 
     def destroy
@@ -38,13 +71,6 @@ module Rapidfire
         QuestionGroupResults.new(question_group: @question_group).extract
 
       respond_with(@question_group_results, root: false)
-    end
-
-    def update
-      @question_group = QuestionGroup.find(params[:id])
-      @question_group.save
-
-      respond_with(@question_group, location: rapidfire.question_groups_url)      
     end
 
     private
--- app/models/rapidfire/question_group.rb
+++ app/models/rapidfire/question_group.rb
@@ -3,8 +3,16 @@ module Rapidfire
     has_many  :questions
     validates :name, :teacher, :presence => true
 
+	def self.search(search)
+	   if search
+	     search_condition = "%" + search + "%"
+	     where(['code LIKE ? OR teacher LIKE ?', search_condition, search_condition])
+	   end
+	end	
+
     if Rails::VERSION::MAJOR == 3
       attr_accessible :name
     end
+
   end
 end
