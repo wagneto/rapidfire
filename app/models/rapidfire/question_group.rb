b3f6c4407451d156b5843f4fe8129d94e9e70cb5
Wagner Matos <eu@wagneto.com.br>
2014-11-16 12:59:26 -0200

    Inserindo pesquisa de administrador

--- app/models/rapidfire/question_group.rb
+++ app/models/rapidfire/question_group.rb
@@ -3,12 +3,19 @@ module Rapidfire
     has_many  :questions
     validates :name, :teacher, :presence => true
 
-	def self.search(search)
-	   if search
-	     search_condition = search
-	     where(['code = ?', search_condition])
-	   end
-	end	
+  	def self.search(search)
+  	   if search
+  	     search_condition = search
+  	     where(['code = ?', search_condition])
+  	   end
+  	end	
+
+    def self.search_admin(search)
+       if search
+         search_condition = search
+         where(['LOWER(teacher) LIKE ?', "%#{search_condition}%"])
+       end
+    end 
 
     if Rails::VERSION::MAJOR == 3
       attr_accessible :name
